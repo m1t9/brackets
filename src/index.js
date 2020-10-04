78ea7e2303f41e3ab19c5a398df82fc8cd4aac4d
m1t9 <dpleshanov@gmail.com>
2020-10-04 16:44:18 +0300

    feat: Brackets implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	
+	while (true) {
+		let loop = 0;
+		bracketsConfig.forEach(item => {
+			if (str.includes(item[0] + item[1])) {
+				str = str.replace(item[0] + item[1], '');
+				loop += 1;
+			}
+		})
+		if (loop === 0) break;
+	}
+
+	return (str === '') ? true : false;
 }
